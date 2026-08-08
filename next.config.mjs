f7fcdc84bd9e8007713d680b4433f3f2caef8e6d
castaneda-dev <castaneda-dev@users.noreply.github.com>
2026-08-08 08:37:22 -0400

    Revert "feat: migrate landing page HTML to Next.js page.jsx with SSR and un-rewrite root"
    This reverts commit 1a0a305e7f76f211028deb53ae2fa9a3794df20c.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: '/',
+        destination: '/index.html',
+      },
+      {
         source: '/dashboard_admin',
         destination: '/admin',
       },
